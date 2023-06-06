e280698a7f36d906726a766331a7b876c705ac0a
Ali Al-Khamis <ali.h2030@gmail.com>
2023-06-06 23:34:36 +0300

    first commit

---
+++ ExcelEditor/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExcelEditor
+{
+    public partial class App : Application
+    {
+        public App ()
+        {
+            //Syncfusion license key: MjMzNTA5NEAzMjMxMmUzMTJlMzMzNVp1SjRPUERvdjZSSmkrRHlWMWtPSzQxVjlRYXBudXJRZDNEdHJ6Tjg4YzA9
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjMzNTA5NEAzMjMxMmUzMTJlMzMzNVp1SjRPUERvdjZSSmkrRHlWMWtPSzQxVjlRYXBudXJRZDNEdHJ6Tjg4YzA9");
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
